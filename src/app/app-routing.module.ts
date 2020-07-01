d246f0847b43e0cfcc334fa806dbea11c555db16
angularaspirants <angularaspirants@gmail.com>
2020-07-01 18:35:15 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BuyCarComponent} from './comps/buy-car/buy-car.component';
+import {SellCarComponent} from './comps/sell-car/sell-car.component';
+import {PageNotFoundComponent} from './comps/page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BuyCarComponent },
+  { path: 'buy-car', component: BuyCarComponent },
+  { path: 'sell-car', component: SellCarComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
